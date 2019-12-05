1c7c7f488711c5e03128a5ff1a08e374b66ae702
Danil Ulmashev <armaros88@gmail.com>
2019-12-04 19:51:20 -0500

    Initial Commit

---
+++ test2/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  test2
+//
+//  Created by Danil Ulmashev on 2019-12-04.
+//  Copyright © 2019 Danil Ulmashev. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
